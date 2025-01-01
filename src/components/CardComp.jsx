15ac81b5a0b7d0292f1fc16413dec70298668d5f
Nevorse <selcuk0463@gmail.com>
2025-01-01 22:41:55 +0300

    read-0

--- src/components/CardComp.jsx
+++ src/components/CardComp.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useState } from "react";
 
 export default function CardComp({ type = 1, doc }) {
 
@@ -11,16 +12,14 @@ export default function CardComp({ type = 1, doc }) {
             "w-[350px] gap-1 bg-black/5 rounded-md overflow-hidden": type === 1,
             "w-[350px] gap-2": type === 2,
           }
-        )}>
+        )}
+      >
         <div
-          style={{ backgroundImage: `url(${doc?.data[0]})` }}
-          className={classNames(
-            "w-full bg-no-repeat bg-center bg-cover",
-            {
-              "h-[240px]": type === 1,
-              "h-[220px]": type === 2,
-            }
-          )}
+          style={{ backgroundImage: `url(${doc.data && doc.data[0]})` }}
+          className={classNames("w-full bg-no-repeat bg-center bg-cover", {
+            "h-[240px]": type === 1,
+            "h-[220px]": type === 2,
+          })}
         />
         <h5 className="font-bold text-lg text-center text-[color:var(--color-primary)] mt-2">
           {doc?.title}
